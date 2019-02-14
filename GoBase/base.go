6e6ed0e2af50fe66a735b0d7332e0fe63b7b36e8
桂素伟 <axzxs2001@163.com>
2019-02-14 18:31:54 +0900

    控制语句

--- GoBase/base.go
+++ GoBase/base.go
@@ -7,7 +7,7 @@ func main() {
 	// vardemo()
 	// f1()
 	// f2(10)
-	fmt.Println(control(2))
+	fmt.Println(control(8))
 }
 
 //变量测试
@@ -40,11 +40,17 @@ func vardemo() {
 }
 
 func control(a int) int {
+	//1
 	if a > 10 {
 		fmt.Println("if a> 10")
 	} else {
 		fmt.Println("else a> 10")
 	}
+	//2
+	if e := 5; e < a {
+		fmt.Println("e := 5; e < a")
+	}
+	//3
 	switch a {
 	case 1:
 		fmt.Println("switch case 1")
@@ -56,6 +62,43 @@ func control(a int) int {
 		fmt.Println("switch case default")
 		break
 	}
+
+	//4
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	//5
+	list := []string{"a", "b", "c", "d", "e", "f"}
+	for k, v := range list {
+		fmt.Println("list[", k, "] = ", v)
+	}
+	//6
+	for pos, char := range "aΦx" {
+		fmt.Printf("character '%c' starts at byte position %d\n", char, pos)
+	}
+	//7
+	arrs := [3][2]int{[2]int{1, 2}, [2]int{3, 4}, [2]int{5, 6}}
+	for k, v := range arrs {
+		for k1, v1 := range v {
+			fmt.Println("arrs[", k, ",", k1, "] = ", v1)
+		}
+	}
+	//8
+	s0 := []int{0, 0}
+	s1 := append(s0, 2, 3, 4, 5)
+	for k1, v1 := range s1 {
+		fmt.Println("arrs[", k1, "] = ", v1)
+	}
+	fmt.Println("s1长度：", len(s1))
+
+	//9
+	monthdays := map[string]int{"Jan": 31, "Feb": 28, "Mar": 31, "Apr": 30, "May": 31, "Jun": 30, "Jul": 31, "Aug": 31, "Sep": 30, "Oct": 31, "Nov": 30, "Dec": 31}
+	delete(monthdays, "Jan")
+	for key, day := range monthdays {
+		fmt.Println(key, "-", day)
+	}
+
 	return 10
 }
 func f1() {
@@ -91,5 +134,17 @@ for
 import
 return
 var
+
+
+close 用于 channel 通讯。使用它来关闭 channel，参阅第 6 章了解更多。
+delete 用于在 map 中删除实例。
+len和cap 可用于不同的类型，len用于返回字符串、slice和数组的长度。参阅“array、slices 和 map” 小节了解更多关于 slice、数组和函数 cap 的详细信息。
+new用于各种类型的内存分配。参阅 “用 new 分配内存” 的第 55 页。
+make用于内建类型（map、slice 和 channel）的内存分配。参阅 “用 make 分配内存” 的第 55 页。
+copy用于复制 slice。参阅本章的 “slice”。
+append 用于追加 slice。参阅本章的 “slice”。
+panic和recover 用于 异常 处理机制。参阅 “恐慌（Panic）和恢复（Recover）” 的第 32 页了解更 多信息。
+print和println 是底层打印函数，可以在不引入 fmt 包的情况下使用。它们主要用于调试。
+complex 、 real和imag 全部用于处理 复数。有了之前给的简单的例子，不用再进一步讨论复数了。
 
 */
